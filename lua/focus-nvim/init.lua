d373add605ddd75e423c582a520c2a4f94a7d21b
Manuel Stieger <manue.stieger@gmail.com>
2025-04-16 15:30:28 +0200

    restore original foldmethod correctly

--- lua/focus-nvim/init.lua
+++ lua/focus-nvim/init.lua
@@ -10,31 +10,32 @@ function M.foldFunctionsAndMethods()
 	local win = vim.api.nvim_get_current_win()
 
 	vim.api.nvim_set_option_value("foldmethod", "manual", { win = win })
+	vim.schedule(function ()
+		local ft = vim.bo.filetype
+		local queryStr
+		if config.languages[ft] then
+			queryStr = config.languages[ft]
+		else
+			queryStr = config.fallback
+		end
 
-	local ft = vim.bo.filetype
-	local queryStr
-	if config.languages[ft] then
-		queryStr = config.languages[ft]
-	else
-		queryStr = config.fallback
-	end
+		local status, parser = pcall(vim.treesitter.get_parser, buf, ft)
+		if not status then return end
 
-	local status, parser = pcall(vim.treesitter.get_parser, buf, ft)
-	if not status then return end
-
-	local tree = parser:parse()[1]
-	local root = tree:root()
+		local tree = parser:parse()[1]
+		local root = tree:root()
 
 
-	local ok, query = pcall(vim.treesitter.query.parse, ft, queryStr)
-	if not ok then return end
+		local ok, query = pcall(vim.treesitter.query.parse, ft, queryStr)
+		if not ok then return end
 
-	for _, node, _ in query:iter_captures(root, buf, 0, - 1) do
-		local startRow, _, endRow, _ = node:range()
-		vim.cmd(string.format("%d,%dfold", startRow + 1, endRow + 1))
-	end
+		for _, node, _ in query:iter_captures(root, buf, 0, - 1) do
+			local startRow, _, endRow, _ = node:range()
+			vim.cmd(string.format("%d,%dfold", startRow + 1, endRow + 1))
+		end
 
-	vim.api.nvim_set_option_value("foldmethod", originalFoldmethod, { win = win })
+		vim.api.nvim_set_option_value("foldmethod", originalFoldmethod, { win = win })
+	end)
 end
 
 function M.foldAround()
@@ -49,35 +50,35 @@ function M.foldAround()
 	end
 
 	vim.api.nvim_set_option_value("foldmethod", "manual", { win = win })
-
-	local ft = vim.bo.filetype
-	local queryStr
-	if config.languages[ft] then
-		queryStr = config.languages[ft]
-	else
-		queryStr = config.fallback
-	end
-
-	local status, parser = pcall(vim.treesitter.get_parser, buf, ft)
-	if not status then return end
-
-	local tree = parser:parse()[1]
-	local root = tree:root()
-
-	local ok, query = pcall(vim.treesitter.query.parse, ft, queryStr)
-	if not ok then return end
-
-	for _, node, _ in query:iter_captures(root, buf, lastLine - 1, lastLine) do
-		local startRow, _, endRow, _ = node:range() -- startRow & endRow are 0-indexed
-		if startRow < line and line < endRow + 2 then
-			goto continue
+	vim.schedule(function ()
+		local ft = vim.bo.filetype
+		local queryStr
+		if config.languages[ft] then
+			queryStr = config.languages[ft]
+		else
+			queryStr = config.fallback
 		end
-		vim.cmd(string.format("%d,%dfold", startRow + 1, endRow + 1))
-		::continue::
-	end
 
-	lastLine = line
-	vim.api.nvim_set_option_value("foldmethod", originalFoldmethod, { win = win })
+		local status, parser = pcall(vim.treesitter.get_parser, buf, ft)
+		if not status then return end
+
+		local tree = parser:parse()[1]
+		local root = tree:root()
+
+		local ok, query = pcall(vim.treesitter.query.parse, ft, queryStr)
+		if not ok then return end
+
+		for _, node, _ in query:iter_captures(root, buf, lastLine - 1, lastLine) do
+			local startRow, _, endRow, _ = node:range() -- startRow & endRow are 0-indexed
+			if startRow < line and line < endRow + 2 then
+				goto continue
+			end
+			vim.cmd(string.format("%d,%dfold", startRow + 1, endRow + 1))
+			::continue::
+		end
+		lastLine = line
+		vim.api.nvim_set_option_value("foldmethod", originalFoldmethod, { win = win })
+	end)
 end
 
 function M.setup(opts)
@@ -86,8 +87,7 @@ end
 
 vim.api.nvim_create_autocmd("BufReadPre", {
 	callback = function ()
-		local win = vim.api.nvim_get_current_win()
-		originalFoldmethod = vim.api.nvim_get_option_value("foldmethod", { win = win })
+		originalFoldmethod = vim.opt.foldmethod._value
 		vim.cmd("normal! zR")
 		vim.schedule(function ()
 			M.foldFunctionsAndMethods()
