bda384aff4f8144cabccc9ef19bbb4ff6b8f3903
Manuel Stieger <manue.stieger@gmail.com>
2025-04-16 16:21:30 +0200

    add signs

--- lua/focus-nvim/defaults.lua
+++ lua/focus-nvim/defaults.lua
@@ -1,6 +1,43 @@
+local function defaultFormat(errors, warns, infos, hints)
+	local segments = {}
+	if errors > 0 then
+		table.insert(segments, "Errors: " .. errors)
+	end
+	if warns > 0 then
+		table.insert(segments, "Warns: " .. warns)
+	end
+	if infos > 0 then
+		table.insert(segments, "Infos: " .. infos)
+	end
+	if hints > 0 then
+		table.insert(segments, "Hints: " .. hints)
+	end
+
+	local result = ""
+	for _, segment in ipairs(segments) do
+		if result == "" then
+			result = segment
+			goto continue
+		end
+
+		result = result .. ", " .. segment
+		::continue::
+	end
+
+	if result == "" then
+		return ""
+	end
+
+	local virtualText = vim.diagnostic.config().virtual_text or {}
+	result = string.rep(" ", virtualText.spacing) .. virtualText.prefix .. " " .. result
+	return result
+end
+
 return {
 	languages = {
 		["lua"] = "(function_declaration) @func"
 	},
 	fallback = "(function_definition) @func",
+	callback = defaultFormat,
+	hlGroup = "NonText"
 }
--- lua/focus-nvim/init.lua
+++ lua/focus-nvim/init.lua
@@ -1,9 +1,85 @@
 local defaults = require("focus-nvim.defaults")
 local M = {}
 
-local config
+local origHandler = vim.lsp.handlers["textDocument/publishDiagnostics"]
 local originalFoldmethod
+local config
 local lastLine
+local diags
+
+local function updateFoldDiagnostics()
+	if not diags then
+		return
+	end
+
+	local bufnr = vim.api.nvim_get_current_buf()
+	local ns = vim.api.nvim_create_namespace("fold_diagnostics")
+	vim.api.nvim_buf_clear_namespace(bufnr, ns, 0, - 1)
+
+	local totalLines = vim.api.nvim_buf_line_count(bufnr)
+
+	local i = 1
+	while i <= totalLines do
+		local foldStart = vim.fn.foldclosed(i)
+		if foldStart == -1 then
+			i = i + 1
+		else
+			local foldEnd = vim.fn.foldclosedend(i)
+			local errorCount = 0
+			local warnCount = 0
+			local infoCount = 0
+			local hintCount = 0
+
+			for _, diag in ipairs(diags) do
+				if diag.lnum >= (foldStart - 1) and diag.lnum <= (foldEnd - 1) then
+					if diag.severity == vim.diagnostic.severity.ERROR then
+						errorCount = errorCount + 1
+					elseif diag.severity == vim.diagnostic.severity.WARN then
+						warnCount = warnCount + 1
+					elseif diag.severity == vim.diagnostic.severity.INFO then
+						infoCount = infoCount + 1
+					elseif diag.severity == vim.diagnostic.severity.HINT then
+						hintCount = hintCount + 1
+					end
+				end
+			end
+
+			local severity = vim.diagnostic.severity
+			local virtConfig = vim.diagnostic.config().signs.text or {}
+			local virtText = ""
+			local hl = nil
+
+			if errorCount > 0 then
+				-- virtText = "" vim.fn.sign_getdefined("Error")[1]
+				virtText = virtConfig[severity.ERROR]
+				hl = "DiagnosticError"
+			elseif warnCount > 0 then
+				-- virtText = ""
+				virtText = virtConfig[severity.WARN]
+				hl = "DiagnosticWarn"
+			elseif infoCount > 0 then
+				-- virtText = ""
+				virtText = virtConfig[severity.INFO]
+				hl = "DiagnosticInfo"
+			elseif hintCount > 0 then
+				-- virtText = ""
+				virtText = virtConfig[severity.HINT]
+				hl = "DiagnosticHint"
+			end
+
+			vim.api.nvim_buf_set_extmark(bufnr, ns, foldStart - 1, - 1, {
+				virt_text = { {config.callback(errorCount, warnCount, infoCount, hintCount), config.hlGroup }},
+				virt_text_pos = "inline"
+			})
+			vim.api.nvim_buf_set_extmark(bufnr, ns, foldStart - 1, 0, {
+				sign_text = virtText,
+				sign_hl_group = hl,
+			})
+
+			i = foldEnd + 1
+		end
+	end
+end
 
 function M.foldFunctionsAndMethods()
 	local buf = vim.api.nvim_get_current_buf()
@@ -85,9 +161,21 @@ function M.setup(opts)
 	config = vim.tbl_extend("force", defaults, opts)
 end
 
+vim.lsp.handlers["textDocument/publishDiagnostics"] = function(err, result, ctx, config1)
+	origHandler(err, result, ctx, config1)
+
+	local bufnr = vim.api.nvim_get_current_buf()
+	diags = vim.diagnostic.get(bufnr)
+end
+
 vim.api.nvim_create_autocmd("BufReadPre", {
 	callback = function ()
 		originalFoldmethod = vim.opt.foldmethod._value
+	end
+})
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+	callback = function ()
 		vim.cmd("normal! zR")
 		vim.schedule(function ()
 			M.foldFunctionsAndMethods()
